0e56e4839b4335213733034e59cf7a3c277d3cf7
sarucm1017 <98sarucm@gmail.com>
2024-08-03 15:35:31 +0530

    changed

--- src/Componets/Layout/ReportSection/Patients/PatientsReports.jsx
+++ src/Componets/Layout/ReportSection/Patients/PatientsReports.jsx
@@ -22,7 +22,7 @@ const PatientReport = () => {
                 <div className="right_section"></div>
               </div>
               <div className="home_bodySection">
-                <div className="col-md-12">
+                <div className="col-md-12 content_body_section">
                   <div className="col-md-6 single_reports">
                     <div className="top_section">
                       <div className="top_section_leftside">
@@ -39,7 +39,22 @@ const PatientReport = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="col-md-6"></div>
+                  <div className="col-md-6 single_reports">
+                  <div className="top_section">
+                      <div className="top_section_leftside">
+                        <p>Health Information</p>
+                      </div>
+                      <div className="top_section_rightside">
+                        <a href="">Details</a>
+                      </div>
+                    </div>
+                    <div className="bottom_section">
+                      <div className="bottom-section-rightside">
+                        <p>By</p>
+                        <p>Dr.Arya</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
