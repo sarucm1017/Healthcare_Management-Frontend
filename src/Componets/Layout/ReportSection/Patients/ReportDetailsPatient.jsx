578bedf2eaf525329baa2cb5d19c6ffe1626d322
sarucm1017 <98sarucm@gmail.com>
2024-08-05 10:54:08 +0530

    report details

--- src/Componets/Layout/ReportSection/Patients/ReportDetailsPatient.jsx
+++ src/Componets/Layout/ReportSection/Patients/ReportDetailsPatient.jsx
@@ -22,54 +22,54 @@ const ReportDetails = () => {
               </div>
               <div className="">
                 <div className="col-md-12 content-body-section">
-                  <div className=" col-md-6 body_content-section">
-                    <div>
+                  <div className=" col-md-5 body_content-section">
+                    <div className="report_maininput">
                       <p>Blood Group</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Pulse Rate</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Breathing Rate</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Body Temperature</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Blood Pressure</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Oxygen Saturation</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
                   </div>
-                  <div className=" col-md-6 body_content-section">
-                    <div>
+                  <div className=" col-md-5 body_content-section">
+                  <div className="report_maininput">
                       <p>Blood Group</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Pulse Rate</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Breathing Rate</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Body Temperature</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Blood Pressure</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
-                    <div>
+                    <div className="report_maininput">
                       <p>Oxygen Saturation</p>
                       <div className="report_inputBox"><p></p></div>
                     </div>
