21b22b1ab5c5cc15828eb30b6df2ab85378e09a4
sarucm1017 <98sarucm@gmail.com>
2024-08-16 12:46:36 +0530

    report slice

---
+++ src/Redux/Slices/ReportSlice.jsx
@@ -0,0 +1,41 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const createReport = createAsyncThunk(
+    'report/createReport',async (reportData, {rejectWithValues}) => {
+        try {
+            const reponse = await axios.post("http://localhost:5001/doctor/report", reportData);
+            return reponse.data;
+        } catch (error) {
+            return rejectWithValue(error.response.data); 
+        }
+    }
+);
+
+
+const reportSlice = createSlice({
+    name: 'report',
+  initialState: {
+    report: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createReport.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createReport.fulfilled, (state, action) => {
+        state.loading = false;
+        state.report = action.payload;
+      })
+      .addCase(createReport.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default reportSlice.reducer;
